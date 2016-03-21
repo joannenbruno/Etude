97f22bc28649a0669210fda8c1dee9b12369f04a
Jac21 <jcantu521@gmail.com>
2016-03-21 10:11:58 -0500

    Card add based on submit, TODO: refactor jQuery, make image upload work)

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -16,15 +16,40 @@ $(document).ready(function(){
 
 		// assign to project object
 		var newProject = {
-			'name' : projectNameValue,
-			'description' : projectDescriptionValue,
-			'art' : projectArtValue
+			'name': projectNameValue,
+			'description': projectDescriptionValue,
+			'art': projectArtValue
 		};
 
 		// log event and field values to the console for debug purposes
 		console.log(e);
 		console.log(newProject);
-	};
+
+		// parent level card class elements
+		var newDivColClass = $("<div class = 'col s12 m6' />"),
+				newDivCardClass = $("<div class = 'card medium' />"),
+				newDivCardImageClass = $("<div class = 'card-image' />"),
+				newCardImgSrc = $("<img src=" + newProject.art + ">");
+
+		// inner card elements
+		var newDivCardContentClass = $("<div class = 'card-content' />"),
+				newSpanCardTitleClass = $("<span class = 'card-title text-white'>" + newProject.name + "</span>"),
+				newCardParagraph = $("<p>" + newProject.description + "</p>"),
+				newDivCardActionClass = $("<div class = 'card-action' />"),
+				newHrefProjectPage = $("<a href='project.html'>Take me to the repo tree</a>");
+
+		// appending
+		$('#project-row').append(newDivColClass);
+		$(newDivColClass).append(newDivCardClass);
+		$(newDivCardClass).append(newDivCardImageClass);
+		$(newDivCardImageClass).append(newCardImgSrc);
+
+		$(newDivCardClass).append(newDivCardContentClass);
+		$(newDivCardContentClass).append(newSpanCardTitleClass);
+		$(newSpanCardTitleClass).after(newCardParagraph);
+		$(newDivCardClass).append(newDivCardActionClass);
+		$(newDivCardActionClass).append(newHrefProjectPage);
+	}
 
 	// track modal submit function
 	$('#track-submit').on('click', addTrack);
@@ -39,14 +64,14 @@ $(document).ready(function(){
 
 		// assign to track object
 		var newTrack = {
-			'title' : trackTitleValue,
-			'description' : trackDescriptionValue,
-			'version-number' : trackVersionNumberValue,
-			'file' : trackFile
+			'title': trackTitleValue,
+			'description': trackDescriptionValue,
+			'version-number': trackVersionNumberValue,
+			'file': trackFile
 		};
 
 		// log event and fields values to the console for debugging purposes
 		console.log(e);
 		console.log(newTrack);
-	};
+	}
 });
