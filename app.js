8d01b59eed0751ca915cdfe0a603ec44ac49ee7a
Jac21 <jcantu521@gmail.com>
2016-10-15 18:18:18 -0500

    Added server backend, including auth routes and user mongoose model

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 'use strict';
 
-// simple express server
+// dependencies
 var express = require('express');
-var app = express();
-var router = express.Router();
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressSession = require('express-session');
+var mongoose = require('mongoose');
+var hash = require('bcrypt-nodejs');
+var path = require('path');
+var passport = require('passport');
+var localStrategy = require('passport-local' ).Strategy;
 
-// set static folder
-app.use(express.static('public'));
+// mongoose
+mongoose.connect('mongodb://localhost/etude');
+
+// user schema/model
+var User = require('./server/models/user.js');
+
+// create instance of express
+var app = express();
+
+// require routes
+var routes = require('./server/routes/api.js');
+
+// define middleware
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'melody',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// configure passport
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
+app.use('/user/', routes);
 
 // get static pages
 app.get('/', function(req, res) {
@@ -29,8 +67,19 @@ app.get('/track', function(req, res) {
 	res.sendFile('./public/track.html', {root : __dirname});
 });
 
-// set port and start server
-var port = 8080;
-app.listen(port, function() {
-	console.log("Listening on port " + port);
+// error hndlers
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
+
+app.use(function(err, req, res) {
+  res.status(err.status || 500);
+  res.end(JSON.stringify({
+    message: err.message,
+    error: {}
+  }));
+});
+
+module.exports = app;
---
+++ server/models/user.js
@@ -0,0 +1,18 @@
+// user model
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+/*
+	Schemas
+*/
+var userSchema = new Schema({
+  username: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
